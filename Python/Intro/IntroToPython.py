70c4238750173c4ae252912f09e33f375edd572f
Joey <JPDengler@users.noreply.github.com>
2024-07-11 16:17:24 -0400

    Create IntroToPython.py

---
+++ Python/Intro/IntroToPython.py
@@ -0,0 +1,17 @@
+# Create a variable name and assign your name to it. 
+# Then, print a greeting message that includes your name.
+name = 'joey'
+print(f"Hello, " + name)
+
+var1 = 5
+var2 = 10
+varSum = var1 + var2
+varDiff = var1 - var2
+varProd = var1 * var2
+varQuo = var1 / var2
+
+print(f"The Numbers {var1} and {var2}")
+print(f"Sum = {varSum}" + 
+      f" Diff = {varDiff}" +
+      f" Prod = {varProd}" +
+      f" Quo = {varQuo}")
